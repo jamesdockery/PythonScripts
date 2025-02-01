20c41bcd750fa032dd790b37ec1137ba84a0bae9
James Dockery <44484428+jamesdockery@users.noreply.github.com>
2025-02-01 07:48:49 -0500

    More small programs

---
+++ SampleInput.py
@@ -0,0 +1,8 @@
+# Write your code here :-)
+print('What is your name?')
+name = input()
+
+if name == 'Alice':
+    print('Hi, Alice')
+else:
+    print('Hi, Stranger')
---
+++ Vampire.py
@@ -0,0 +1,12 @@
+# Write your code here :-)
+name = 'Carol'
+age = 30
+if name == 'Alice':
+    print('Hi, Alice.')
+elif age < 12:
+    print ('You are not Alice, kiddo.')
+elif age > 20:
+    print('Unlike you, Alice is not an undead, immortal vampire.')
+elif age > 100:
+    print('You are not Alice, grannie.')
+
